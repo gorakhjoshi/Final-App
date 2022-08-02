fee05f9881dba3094e6cfe5540366f54da19c837
Gorakh Raj joshi <gorakh626@gmail.com>
2022-08-02 06:23:30 +0545

    feat: added helper function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { generateData } from "shared/helpers/data-generation";
+
+console.log(generateData(10));
+
 export default function App() {
   return <div className="App">hello world</div>;
 }
---
+++ src/shared/helpers/http-mocks.ts
@@ -0,0 +1,17 @@
+import { getRandom } from "./math.utils";
+interface Options {
+  success?: boolean;
+  randomFailure?: boolean;
+}
+
+export function httpMocks({ success, randomFailure }: Options) {
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.99 && success) {
+        resolve();
+      } else {
+        reject({ message: "Failed" });
+      }
+    }, getRandom(10, 20) * 100);
+  });
+}
