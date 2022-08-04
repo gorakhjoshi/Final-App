7008e3b99180ee6d2f4a387c7715b6d077f0e4c2
Gorakh Raj joshi <gorakh626@gmail.com>
2022-08-04 06:34:22 +0545

    fix: updated interfaces

--- src/shared/interfaces/http.interface.ts
+++ src/shared/interfaces/http.interface.ts
@@ -1,4 +1,22 @@
+export type ApiResponse<DataType> =
+  | (DataType & SuccessResponse)
+  | FailureResponse;
+
 export interface SuccessResponse {
   success: true;
   error?: undefined;
+}
+export interface FailureResponse {
+  success: false;
+  error: ResponseError;
+}
+export interface ResponseError {
+  type?: string;
+  message?: string;
+}
+
+export enum HTTPMethod {
+  get = "GET",
+  post = "POST",
+  put = "PUT",
 }
