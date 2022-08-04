5f46f79a0bc43fb313af5d367132c83fb2cc8b16
Gorakh Raj joshi <gorakh626@gmail.com>
2022-08-04 06:37:53 +0545

    fix: updated models

--- src/shared/models/roll.ts
+++ src/shared/models/roll.ts
@@ -1,1 +1,12 @@
-export type RollStateType = "unmark" | "present" | "absent" | "late";
+export interface Roll {
+  id: number;
+  name: string;
+  completed_at: Date;
+  student_roll_states: { student_id: number; roll_state: RolllStateType }[];
+}
+
+export interface RollInput {
+  student_roll_states: { student_id: number; roll_state: RolllStateType }[];
+}
+
+export type RolllStateType = "unmark" | "present" | "absent" | "late";
